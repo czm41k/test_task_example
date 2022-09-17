9150254277c31587e4af80c6ccfd79f9f1723526
Evgeny Tselikov <evgeny.tselikov@exness.com>
2022-09-17 16:11:49 +0300

    linting

--- app-code/app.py
+++ app-code/app.py
@@ -1,10 +1,13 @@
+import os
+import json
+import requests
 from flask import Flask, request, jsonify
-import os,json, requests
-import logging
+# import logging
 
 
 WEATHER_URL = os.getenv('WEATHER_URL',default='https://api.openweathermap.org/data/2.5/weather')
-WEATHER_CITY_ID = os.getenv('WEATHER_CITY_ID',default='2643743') # London city id http://bulk.openweathermap.org/sample/city.list.json.gz
+# London city id http://bulk.openweathermap.org/sample/city.list.json.gz
+WEATHER_CITY_ID = os.getenv('WEATHER_CITY_ID',default='2643743')
 WEATHER_API_TOKEN = os.getenv("WEATHER_TOKEN")
 
 app = Flask(__name__)
@@ -26,8 +29,8 @@ def html_response():
                     <h2>Temperature(Fahrenheit) is {resp['main']['temp']}, but feels like {resp['main']['feels_like']}. Day's range is between {resp['main']['temp_min']} and {resp['main']['temp_max']} </h2>
                     <h2> Also expect {resp['weather'][0]['description']} and wind's speed up to {resp['wind']['speed']} </h2>
                     """
-        except Exception as e:
-            return "<h1> Error </h1>\n<h2>Sorry, but Failed to get weather</h2>"
+        except requests.exceptions.RequestException as error:
+            return f"<h1> Error </h1>\n<h2>Sorry, but Failed to get weather. Error is {error}</h2>"
     else:
         return "<h1>PONG</h1>"
 
@@ -35,7 +38,7 @@ def html_response():
 def check_weather():
     """Calls external API to get weather data"""
     url = f"{WEATHER_URL}?id={WEATHER_CITY_ID}&appid={WEATHER_API_TOKEN}"
-    weather_resp = requests.get(url)
+    weather_resp = requests.get(url,timeout=5)
     return json.loads(weather_resp.text)
 
 
