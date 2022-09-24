d3bd874d5295459b17b4324a24dae9213876379f
Evgeny Tselikov <evgeny.tselikov@exness.com>
2022-09-24 18:53:55 +0300

    list->tolist

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ data "aws_partition" "current" {}
 
 locals {
   cluster_name = "${var.owner}-eks-${var.env}"
-  identifiers = list(concat(
+  identifiers = tolist(concat(
     var.access_list,
     ["arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"]
   ))
