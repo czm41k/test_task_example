7a06bfb286547175cdb1fe9da9e61e504c8975c5
Evgeny Tselikov <evgeny.tselikov@exness.com>
2022-09-24 21:52:17 +0300

    deleting useless staff

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -8,7 +8,7 @@ module "eks" {
   subnet_ids      = module.vpc.private_subnets
   eks_managed_node_group_defaults = {
     ami_type                              = "AL2_x86_64"
-    attach_cluster_primary_security_group = true
+    attach_cluster_primary_security_group = false
     # Disabling and using externally provided security groups
     create_security_group = false
   }
@@ -19,9 +19,6 @@ module "eks" {
       min_size       = 0
       max_size       = 1
       desired_size   = 1
-      vpc_security_group_ids = [
-        aws_security_group.worker_node_group.id
-      ]
     }
   }
   tags = {
--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -1,14 +1,14 @@
-resource "aws_security_group" "worker_node_group" {
-  name_prefix = "worker_node_group"
-  vpc_id      = module.vpc.vpc_id
+# resource "aws_security_group" "worker_node_group" {
+#   name_prefix = "worker_node_group"
+#   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+#   ingress {
+#     from_port = 22
+#     to_port   = 22
+#     protocol  = "tcp"
 
-    cidr_blocks = [
-      "10.0.0.0/8",
-    ]
-  }
-}
+#     cidr_blocks = [
+#       "10.0.0.0/8",
+#     ]
+#   }
+# }
