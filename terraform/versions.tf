160fb3723db7794b4bde2bae48165b320ad4fa81
Evgeny Tselikov <evgeny.tselikov@exness.com>
2022-09-24 16:46:24 +0300

    tried to change vers

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,7 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.15.0"
     }
-
+    ecr = {
+      source  = "hashicorp/aws"
+      version = "1.4.0"
+    }
     random = {
       source  = "hashicorp/random"
       version = "3.1.0"
