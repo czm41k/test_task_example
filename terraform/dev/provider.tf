7dc09ab3e26942592bd007f7f687c9cc2de9e16b
Evgeny Tselikov <evgeny.tselikov@exness.com>
2022-09-25 13:45:08 +0300

    parametrized owner + envr

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -10,10 +10,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "czm41k-terraform-state"
+    bucket         = "czm41k-terraform-state-dev"
     key            = "terraform.tfstate"
     region         = "us-west-1"
-    dynamodb_table = "czm41k-terraform-state-lock"
+    dynamodb_table = "czm41k-terraform-state-dev-lock"
   }
   required_version = "1.3.0"
 }
